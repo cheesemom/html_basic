d6ab64acb423d1d8c0a4e725f85562cb4da34d94
cheesemom <smsm2610@naver.com>
2022-08-22 14:11:40 +0900

    0822

---
+++ js/section04.js
@@ -0,0 +1,10 @@
+const Section04 = () => {
+    return (
+        <section className='About02 basicSection'>
+            <h2><span>CEO's</span>GREETINGS</h2>
+            <p>귀여운 산리오 쿠로미</p>
+        </section>
+    )
+}
+
+export default Section04;
